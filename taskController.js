d6e19521810b49030780eb2be7ddfac763d060ff
SANDARAPOT <sandaraperez1706@gmail.com>
2024-12-02 08:33:24 +0800

    Add files via upload

---
+++ taskController.js
@@ -0,0 +1,76 @@
+// controllers/itemController.js
+const task= require("../models/taskModel");
+
+// Get all task
+const getAllTasks = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .json({ message: "Tasks  retrieved successfully", data: task });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+};
+
+
+// Add a new task
+const addTask = async (req, res) => {
+  try {
+    const newTask = req.body;
+    tasks.push(newTask);
+    res.status(201).json({ message: "Task added successfully", data: task });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+};
+
+// Update an existing task
+const updateTask = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const index = tasks.findIndex((task) => task.id === id);
+    if (index !== -1) {
+      tasks[index] = { ...tasks[index], ...req.body };
+      res
+        .status(200)
+        .json({ message: "Task updated successfully", data: tasks });
+    } else {
+      res.status(404).json({ message: "Task not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+};
+
+// Delete an tasks
+const deleteTask= async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const index = tasks.findIndex((task) => task.id === id);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      res
+        .status(200)
+        .json({ message: "Task deleted successfully", data: tasks});
+    } else {
+      res.status(404).json({ message: "Task not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
+  }
+};
+
+module.exports = {
+  getAllTasks,
+  addTask,
+  updateTask,
+  deleteTask,
+};
---
+++ taskRoutes.js
@@ -0,0 +1,17 @@
+// routes/taskRoutes.js
+const express = require("express");
+const router = express.Router();
+const {
+  getAllTasks,
+  addTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
+
+// Route definitions
+router.get("/", getAllTasks);
+router.post("/", addTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+module.exports = router;
